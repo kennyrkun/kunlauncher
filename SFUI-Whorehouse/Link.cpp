353871c1dd7e741a796c53cd459c75531cabe116
Kenny McCormick <keennymccormic+github@gmail.com>
2017-09-14 21:38:57 -0500

    fixed link positioning

--- SFUI-Whorehouse/Link.cpp
+++ SFUI-Whorehouse/Link.cpp
@@ -1,17 +1,14 @@
-
 #include <fstream>
 #include <iostream>
 #include <experimental\filesystem>
 
 #include "Globals.hpp"
-
 #include "Link.hpp"
 #include "Download.hpp"
 
 Link::Link(std::string text, std::string uri, sf::RenderWindow* target_window, float xPos)
 {
 	targetWindow = target_window;
-
 	linkText.setString(text);
 	linkRel = uri;
 
@@ -20,17 +17,16 @@ Link::Link(std::string text, std::string uri, sf::RenderWindow* target_window, f
 	cardShape.setSize(sf::Vector2f(targetWindow->getSize().x - 25, 40));
 	cardShape.setOrigin(sf::Vector2f(cardShape.getLocalBounds().width / 2, cardShape.getLocalBounds().height / 2));
 	cardShape.setPosition(sf::Vector2f((targetWindow->getSize().x / 2) - 5, xPos)); // probably not the best
-	cardShape.setFillColor(sf::Color(100, 100, 100));
+	cardShape.setFillColor(CONST::COLOR::LINK::CARD);
 
 	font.loadFromFile("C:\\Windows\\Fonts\\Arial.ttf");
 	linkText.setFont(font);
-//	linkText.setPosition(sf::Vector2f(cardShape.getPosition().x - 240, cardShape.getPoint(0).y + 5));
-	linkText.setPosition(sf::Vector2f(cardShape.getPosition().x - 242, cardShape.getPosition().y - 22));
+	linkText.setPosition(static_cast<int>((cardShape.getPosition().x - cardShape.getLocalBounds().width / 2) + 15), static_cast<int>(cardShape.getPosition().y - cardShape.getLocalBounds().height / 2));
 	linkText.setStyle(sf::Text::Underlined);
-	linkText.setFillColor(sf::Color(0, 170, 232));
+	linkText.setFillColor(CONST::COLOR::LINK::TEXT); // standard link blue, I think.
 
 	// don't center them because they may get longer or shorter, but they need to be left aligned
-	followLinkTexture.loadFromFile(".\\" + CONST::DIR::BASE + "\\" + CONST::DIR::RESOURCE + "\\" + CONST::DIR::TEXTURE + "\\input_1x.png");
+	followLinkTexture.loadFromFile(".\\" + CONST::DIR::BASE + CONST::DIR::RESOURCE + CONST::DIR::TEXTURE + "input_1x.png");
 	followLinkTexture.setSmooth(true);
 	followLinkButton.setTexture(&followLinkTexture);
 	followLinkButton.setSize(sf::Vector2f(24, 24));
@@ -69,7 +65,7 @@ void Link::update()
 
 	totalHeight = cardShape.getLocalBounds().height;
 
-	linkText.setPosition(sf::Vector2f(cardShape.getPosition().x - 242, cardShape.getPosition().y - 22));
+	linkText.setPosition(static_cast<int>((cardShape.getPosition().x - cardShape.getLocalBounds().width / 2) + 15), static_cast<int>(cardShape.getPosition().y - cardShape.getLocalBounds().height / 2));
 
 	followLinkButton.setSize(sf::Vector2f(24, 24));
 	followLinkButton.setOrigin(sf::Vector2f(followLinkButton.getLocalBounds().width / 2, followLinkButton.getLocalBounds().height / 2));
@@ -97,17 +93,4 @@ std::ifstream& Link::GotoLine(std::ifstream& file, unsigned int num)
 	}
 
 	return file; // return line
-}
-
-void Link::getLink(std::string from_string)
-{
-	std::string temp = from_string;
-	temp.erase(0, temp.find_first_of('"') + 1);
-	temp.erase(temp.find_first_of('"'), temp.length());
-	linkText.setString(temp);
-
-	temp = from_string;
-	temp.erase(0, temp.find_first_of("=") + 2);
-	temp.erase(temp.find_last_of('"'), temp.length());
-	linkRel = temp;
 }
